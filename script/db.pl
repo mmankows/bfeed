097497ad8ee507fcb86e9569818c5fe61ef18767
maniek <maniek.et@gmail.com>
2015-11-16 19:26:08 +0100

    db scripts

--- script/db.pl
+++ script/db.pl
@@ -1,4 +1,12 @@
 #!/usr/bin/perl
-qx(cat script/create_database.sql|mysql -uroot);
+use lib 'lib';
+use BFeed::Config;
+$cfg = BFeed::Config->new();
+($user, $pwd) = $cfg->db_user, $cfg->db_pass;
+warn "Setting up database & data...\n";
+warn "User: $user  Pass: $pwd\n";
+$pwd = $pwd ? "-p$pwd" : '';
+
+qx(cat script/create_database.sql|mysql -u$user $pwd);
 qx(perl script/db_to_model.pl);
-qx(cat script/sample_data.sql|mysql -uroot);
+qx(cat script/sample_data.sql|mysql -u$user -$pwd);
